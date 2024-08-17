32c06d3e0d456d92d5f72a730e8bd60fb7d6d4e4
Renato Sousa <renatosousa@sousadev.me>
2024-08-17 12:15:12 -0300

    adicionando config basica do jest

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+export default {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'tsx', 'js'],
+    testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest',
+    },
+  };
