857366cf6febbd363d13eb92bd8a25c113d76105
Renato Sousa <renatosousa@sousadev.me>
2024-08-16 21:26:54 -0300

    testando outra forma de exibir a porta do socket

--- src/server.ts
+++ src/server.ts
@@ -17,9 +17,16 @@ const wss = new WSServer({ server });
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-wss.on('connection', (ws: WebSocket) => {
+wss.on('connection', (ws: WebSocket, req) => {
   console.log('Client connected');
 
+  const clientPort = req.socket.remotePort; // Pega a porta do cliente
+
+  console.log('Client connected on port:', clientPort);
+
+  // Envia a porta do cliente para ele mesmo
+  ws.send(`You connected from port: ${clientPort}`);
+  
   ws.on('close', () => console.log('Client disconnected'));
 });
 
